45a1c13e2725341511f8fab18b50c1798b78a327
subr1990 <subramaniank@live.com>
2020-09-05 00:05:56 +1000

    fdg
    dfg

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://webboot123.herokuapp.com/api/test/';
 
 @Injectable({
   providedIn: 'root'
