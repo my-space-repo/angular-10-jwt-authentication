4aa7c1634819133291d6693b41ae6f636fdc542d
subr1990 <subramaniank@live.com>
2020-09-05 17:08:39 +1000

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/sng123'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/sng123/index.html'));});
+    '/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
