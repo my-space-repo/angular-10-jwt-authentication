09eed0aee5c0879736a26e1e219373fc49428730
subr1990 <subramaniank@live.com>
2020-09-05 18:23:56 +1000

    FDG

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'https://cors-anywhere.herokuapp.com/https://webboot123.herokuapp.com/api/auth/';
+const AUTH_API = 'https://webboot123.herokuapp.com/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://cors-anywhere.herokuapp.com/https://webboot123.herokuapp.com/api/test/';
+const API_URL = 'https://webboot123.herokuapp.com/api/test/';
 
 @Injectable({
   providedIn: 'root'
